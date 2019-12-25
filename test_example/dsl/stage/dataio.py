0fa85aff5af0e00af54974921608c6b66bb8a826
weiwee <wbwmat@gmail.com>
2019-12-25 16:53:22 +0800

    adjust dataio
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- test_example/dsl/stage/dataio.py
+++ test_example/dsl/stage/dataio.py
@@ -17,11 +17,28 @@ from test_example.dsl.stage.base import Stage
 
 
 class DataIO(Stage):
-    def __init__(self, name=None):
+    def __init__(self, name=None, with_label=True, label_name=None, label_type="int", output_format="dense"):
         super().__init__(name=name, stage_name="DataIO")
         self.global_param = dict(
-            with_label=True,
-            label_name="y",
-            label_type="int",
-            output_format="dense"
+            with_label=with_label,
+            label_type=label_type,
+            output_format=output_format
         )
+        if label_name is not None:
+            self.global_param["label_name"] = label_name = label_name,
+
+    def set_with_label(self, with_label=True, role=None):
+        self.__getattr__("set_with_label")(with_label, role)
+        return self
+
+    def set_label_type(self, label_type="int", role=None):
+        self.__getattr__("set_label_type")(label_type, role)
+        return self
+
+    def set_output_format(self, output_format="dense", role=None):
+        self.__getattr__("set_output_format")(output_format, role)
+        return self
+
+    def set_label_name(self, label_name, role=None):
+        self.__getattr__("set_label_name")(label_name, role)
+        return self
